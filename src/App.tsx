5ce91a7cc0434e875225d210140b65e7cd0fc718
DjaniNanda <djaninandapetrus@gmail.com>
2025-09-11 23:40:04 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
+      <Team />
       <Services />
       <About />
       <Testimonials />
       <Contact />
-      <Team />
       <Footer />
     </div>
   );
